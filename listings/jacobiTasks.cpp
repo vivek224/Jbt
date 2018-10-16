47d8595f0acbb93dc9668fad7fd14ff5b8434690
Vivek Kale <vivek.lkale@gmail.com>
2018-10-16 11:29:11 -0500

    Updating tasks cpp file.

---
+++ listings/jacobiTasks.cpp
@@ -0,0 +1,11 @@
+
+for(int i=0; i<iterations; ++i) {
+
+#pragma omp parallel for
+  for(int y=1; y<(n-1); ++y) {
+  for(int x=(y*n)+1; x<(y*n)+n-1; ++x) {
+  dest[x]=(src[x-n]+src[x+n]+src[x]+src[x-1]+src[x+1])*0.2;
+  }
+  }
+  std::swap(dest, src);
+}
